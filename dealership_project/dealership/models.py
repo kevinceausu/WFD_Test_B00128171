adeba1253224d84137174b486fa8ed6bd348fbfc
kevinceausu <b00128171@mytudublin.ie>
2026-03-23 15:57:55 +0000

    creating more models

--- dealership_project/dealership/models.py
+++ dealership_project/dealership/models.py
@@ -8,4 +8,25 @@ class Salesperson(models.Model):
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
+class Mechanic(models.Model):
+    mechanic_id = models.AutoField(primary_key=True)
+    last_name = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Customer(models.Model):
+    customer_id = models.AutoField(primary_key=True)
+    last_name = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=20)
+    address = models.CharField(max_length=255)
+    city = models.CharField(max_length=100)
+    state_province = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    postal_code = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
